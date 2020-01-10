ccb18c2910a4176e02eb6e59ae108ed68da68467
y437li <li.mac.kaka@gmail.com>
2020-01-10 16:33:54 -0500

    update
    update a lstm classification
    update a lstm regression on Stock's return
    update a GRU regression model on stock's return
    rename lstm model2.py to LSTM regression on stock Price.py

---
+++ MMAI 823 project/LSTM Model Regreesion on Return.py
@@ -0,0 +1,147 @@
+import quandl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.python.client import device_lib
+from keras import backend as K
+from keras import metrics
+import keras as ks
+import pickle
+import pandas_datareader.data as pd_reader
+
+device_lib.list_local_devices()
+K.tensorflow_backend._get_available_gpus()
+######################
+###define treshold####
+######################
+tresh_hold = 0.002
+#######################
+###Lable function #####
+#######################
+def lable(x,tresh_hold):
+    if x >= tresh_hold:
+        return 1
+    else:
+        return 0
+
+######################
+#quandl.ApiConfig.api_key = "uRMo697HgMj91ZZZa2_v"
+#data = quandl.get_table('WIKI/PRICES', qopts = { 'columns': ['ticker', 'date', 'adj_close','open','high','low','close'] }, ticker = ['AAPL'], date = { 'gte': '2009-01-01', 'lte': '2019-12-19' })
+data = pd_reader.DataReader(
+    'AAPL',
+    'yahoo'
+)
+data = data[['Adj Close','High','Open','Close','Low']]
+#data.sort_values(by='Date', ascending=True,inplace =True)
+data["return"] = data["Adj Close"].shift(1) / data["Adj Close"] - 1
+#data["movement"] = data["return"].apply(lambda x:lable(x,tresh_hold))
+data = data.dropna()
+#data.drop(['return'],axis=1,inplace = True)
+factor_ratio = 0.8
+data_1 = data.iloc[:round(len(data)*factor_ratio)]
+data_2 = data[round(len(data)*factor_ratio):]
+
+###############################################
+##  Feature engineering construct the retrun ##
+###############################################
+training_set = data_1.iloc[:,0:-1].values
+y_set = data_1["return"].values
+y_set = y_set.reshape(-1,1)
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+y_set_scaled = sc.fit_transform(y_set)
+
+
+X = []
+y = []
+for i in range(252, len(training_set_scaled)):
+    X.append(training_set_scaled[i-252:i])
+    y.append(y_set_scaled[i][0])
+
+X, y = np.array(X), np.array(y)
+X = np.reshape(X, (X.shape[0], X.shape[1], 5))
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+hidden_layer_sizes = [100,100]
+def build_model(hidden_layer_sizes,act,input_d):
+  model = Sequential()
+
+  model.add(LSTM(units = hidden_layer_sizes[0], activation= act, return_sequences= True,input_shape =input_d))
+  model.add(Dropout(0.01))
+
+  model.add(LSTM(units=hidden_layer_sizes[len(hidden_layer_sizes)-1], activation=act))
+  model.add(Dropout(0.01))
+
+  model.add(Dense(units=25))
+  model.add(Dense(units=1))
+
+  model.compile(optimizer='adam', loss='mean_squared_error')
+  return model
+
+
+
+
+regressor = build_model(hidden_layer_sizes,"relu",(X.shape[1],5))
+
+
+#callbacks = [EarlyStopping(monitor='val_loss', patience=2),
+             #ModelCheckpoint(filepath='best_model.h5', monitor='val_loss', save_best_only=True)]
+
+model = regressor.fit(X, y, epochs = 100, batch_size = 100)
+plt.plot(model.history['loss'])
+plt.show()
+
+###
+
+real_stock_price = data_2['return'].values
+
+dataset_total = data.iloc[:,0:-1]
+inputs = dataset_total[len(dataset_total) - len(data_2) -252:].values
+#inputs = inputs.reshape(-1,1)
+inputs = sc.fit_transform(inputs)
+
+X_test = []
+for i in range(252, len(inputs)):
+    X_test.append(inputs[i-252:i])
+
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 5))
+predicted_stock_price= regressor.predict(X_test)
+y_set_scaled = sc.fit_transform(y_set)
+predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+predicted_stock = pd.DataFrame(predicted_stock_price,columns=['perdict return'])
+real_stock = pd.DataFrame(real_stock_price,columns=['real return'])
+
+final = pd.merge(real_stock, predicted_stock, left_index=True, right_index=True)
+final['direction'] = final["real return"]*final["perdict return"]
+def iden(x):
+    if x >0:
+        return 1
+    else:
+        return 0
+final["hit"] = final["direction"].apply(iden)
+print('hit ratio:{}'.format(final["hit"].sum()/final["hit"].count()))
+
+plt.plot(real_stock_price, color = 'black', label = 'AAPL Stock Price')
+plt.plot(predicted_stock_price, color = 'green', label = 'Predicted AAPL Stock return')
+plt.title('AAPL Stock return Prediction')
+plt.xlabel('Time')
+plt.ylabel('AAPL Stock return')
+plt.legend()
+plt.show()
+
+import pickle
+with open('Apple_model_regression_return.pickle','wb') as f:
+    pickle.dump(regressor,f)
+
+with open('Apple_model_regression_return.pickle','rb') as f:
+    model = pickle.load(f)
+
--- MMAI 823 project/LSTM classification on stock direction.py
+++ MMAI 823 project/LSTM classification on stock direction.py
@@ -15,6 +15,18 @@ import pandas_datareader.data as pd_reader
 
 device_lib.list_local_devices()
 K.tensorflow_backend._get_available_gpus()
+######################
+###define treshold####
+######################
+tresh_hold = 0
+#######################
+###Lable function #####
+#######################
+def lable(x,tresh_hold):
+    if x >= tresh_hold:
+        return 1
+    else:
+        return 0
 
 ######################
 #quandl.ApiConfig.api_key = "uRMo697HgMj91ZZZa2_v"
@@ -23,26 +35,31 @@ data = pd_reader.DataReader(
     'AAPL',
     'yahoo'
 )
-data = data[['Adj Close','High','Low','Low','Close']]
+data = data[['Adj Close','High','Open','Close','Low']]
 #data.sort_values(by='Date', ascending=True,inplace =True)
+data["return"] = data["Adj Close"].shift(1) / data["Adj Close"] - 1
+data["movement"] = data["return"].apply(lambda x:lable(x,tresh_hold))
 data = data.dropna()
-factor_ratio = 0.9
+data.drop(['return'],axis=1,inplace = True)
+factor_ratio = 0.8
 data_1 = data.iloc[:round(len(data)*factor_ratio)]
-data_2 = data[round(len(data)*factor_ratio):]###365
+data_2 = data[round(len(data)*factor_ratio):]
 
 ###############################################
 ##  Feature engineering construct the retrun ##
 ###############################################
-training_set = data_1.iloc[:,0:].values
+training_set = data_1.iloc[:,0:-1].values
+y_set = data_1["movement"].values
 sc = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = sc.fit_transform(training_set)
+#y_set_scaled = sc.fit_transform(y_set)
 
 
 X = []
 y = []
-for i in range(45, len(training_set_scaled)):
-    X.append(training_set_scaled[i-45:i])
-    y.append(training_set_scaled[i,0])
+for i in range(252, len(training_set_scaled)):
+    X.append(training_set_scaled[i-252:i])
+    y.append(y_set[i])
 
 X, y = np.array(X), np.array(y)
 X = np.reshape(X, (X.shape[0], X.shape[1], 5))
@@ -52,22 +69,18 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 
-hidden_layer_sizes = [200,300,300,200,100]
+hidden_layer_sizes = [100,100]
 def build_model(hidden_layer_sizes,act,input_d):
   model = Sequential()
 
-  model.add(LSTM(units = hidden_layer_sizes[0], activation= act,return_sequences= True, input_shape =input_d))
-  model.add(Dropout(0.2))
-
-  for layer_size in hidden_layer_sizes[1:len(hidden_layer_sizes)-1]:
-    model.add(LSTM(units =layer_size, activation = act, return_sequences=True))
-    model.add(Dropout(0.3))
+  model.add(LSTM(units = hidden_layer_sizes[0], activation= act, return_sequences= True,input_shape =input_d))
+  model.add(Dropout(0.01))
 
   model.add(LSTM(units=hidden_layer_sizes[len(hidden_layer_sizes)-1], activation=act))
-  model.add(Dropout(0.2))
+  model.add(Dropout(0.01))
 
   model.add(Dense(units=25))
-  model.add(Dense(units=1,activation=act))
+  model.add(Dense(units=1,activation="softmax"))
 
   model.compile(optimizer='adam', loss='mean_squared_error')
   return model
@@ -81,64 +94,53 @@ regressor = build_model(hidden_layer_sizes,"relu",(X.shape[1],5))
 #callbacks = [EarlyStopping(monitor='val_loss', patience=2),
              #ModelCheckpoint(filepath='best_model.h5', monitor='val_loss', save_best_only=True)]
 
-model = regressor.fit(X, y, epochs = 200, batch_size = 100)
+model = regressor.fit(X, y, epochs = 20, batch_size = 100)
 plt.plot(model.history['loss'])
 plt.show()
 
 ###
 
-real_stock_price = data_2.iloc[:,0:1].values
+real_stock_price = data_2['movement'].values
 
-dataset_total = data.iloc[:,0:]
-inputs = dataset_total[len(dataset_total) - len(data_2) -45:].values
+dataset_total = data.iloc[:,0:-1]
+inputs = dataset_total[len(dataset_total) - len(data_2) -252:].values
 #inputs = inputs.reshape(-1,1)
 inputs = sc.fit_transform(inputs)
 
 X_test = []
-for i in range(45, len(inputs)):
-    X_test.append(inputs[i-45:i])
+for i in range(252, len(inputs)):
+    X_test.append(inputs[i-252:i])
 
 X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 5))
 predicted_stock_price= regressor.predict(X_test)
-sc.fit_transform(real_stock_price)
-predicted_stock_price = sc.inverse_transform(predicted_stock_price)
-predicted_stock_price = pd.DataFrame(predicted_stock_price,columns=['adj_close'])
-predicted_stock_price.reset_index(inplace=True)
-predicted_stock_price = predicted_stock_price[['adj_close']]
-data_test = data[round(len(data)*factor_ratio):]
-data_test.reset_index(inplace=True)
-data_test = data_test[["Date"]]
+#y_set_scaled = sc.fit_transform(y_set)
+#predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+predicted_stock = pd.DataFrame(predicted_stock_price,columns=['perdict direction'])
+real_stock = pd.DataFrame(real_stock_price,columns=['real direction'])
 
-final = pd.merge(data_test, predicted_stock_price, left_index=True, right_index=True)
-#predicted_stock_price['return1'] = predicted_stock_price['close'].shift(1) / predicted_stock_price['close'] - 1
+final = pd.merge(real_stock, predicted_stock, left_index=True, right_index=True)
 
-#predicted_stock_price['cum'] = (1 + predicted_stock_price.return1).cumprod() - 1
-
-
-#d = predicted_stock_price["cum"].values
+def iden(x,y):
+    if x == y:
+        return 1
+    else:
+        return 0
+final["hit"] = final.apply(iden(final['real direction'],final['perdict direction']),axis =1)
+print('hit ratio:{}'.format(final["hit"].sum()/final["hit"].count()))
 
 plt.plot(real_stock_price, color = 'black', label = 'AAPL Stock Price')
-plt.plot(predicted_stock_price['adj_close'], color = 'green', label = 'Predicted AAPL Stock Price')
+plt.plot(predicted_stock_price, color = 'green', label = 'Predicted AAPL Stock return')
 plt.title('AAPL Stock return Prediction')
 plt.xlabel('Time')
 plt.ylabel('AAPL Stock return')
 plt.legend()
 plt.show()
 
-# summarize history for loss
-#plt.plot(regressor.history['loss'])
-#plt.plot(regressor.history['val_loss'])
-#plt.title('model loss')
-##plt.ylabel('loss')
-#plt.xlabel('epoch')
-#plt.legend(['train', 'test'], loc='upper left')
-#plt.show()
-
 import pickle
-with open('Apple_model.pickle','wb') as f:
+with open('Apple_model_classification_return.pickle','wb') as f:
     pickle.dump(regressor,f)
 
-with open('Apple_model.pickle','rb') as f:
+with open('Apple_model_classification_return.pickle','rb') as f:
     model = pickle.load(f)
 
