0afa71d99c7324c969796433fbfb359e2d0f3381
y437li <li.mac.kaka@gmail.com>
2020-01-20 02:57:29 -0500

    LSTM on Return update
    LSTM on Return update

--- MMAI 823 project/LSTM Model Regreesion on Return.py
+++ MMAI 823 project/LSTM Model Regreesion on Return.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,scale,Normalizer
 from sklearn.model_selection import train_test_split
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.python.client import device_lib
@@ -18,26 +18,21 @@ K.tensorflow_backend._get_available_gpus()
 ######################
 ###define treshold####
 ######################
-tresh_hold = 0.002
+tresh_hold = 0
 #######################
 ###Lable function #####
 #######################
-def lable(x,tresh_hold):
-    if x >= tresh_hold:
-        return 1
-    else:
-        return 0
-
+company = 'AAPL'
 ######################
 #quandl.ApiConfig.api_key = "uRMo697HgMj91ZZZa2_v"
 #data = quandl.get_table('WIKI/PRICES', qopts = { 'columns': ['ticker', 'date', 'adj_close','open','high','low','close'] }, ticker = ['AAPL'], date = { 'gte': '2009-01-01', 'lte': '2019-12-19' })
 data = pd_reader.DataReader(
-    'AAPL',
+    '{}'.format(company),
     'yahoo'
 )
-data = data[['Adj Close','High','Open','Close','Low']]
+data = data[['Adj Close','High','Open','Low','Volume']]
 #data.sort_values(by='Date', ascending=True,inplace =True)
-data["return"] = np.log(data["Adj Close"].shift(1))-np.log(data["Adj Close"])
+data["return"] = (data["Adj Close"]-data["Adj Close"].shift(1))/data["Adj Close"].shift(1)
 #data["movement"] = data["return"].apply(lambda x:lable(x,tresh_hold))
 data = data.dropna()
 #data.drop(['return'],axis=1,inplace = True)
@@ -51,7 +46,9 @@ data_2 = data[round(len(data)*factor_ratio):]
 training_set = data_1.iloc[:,0:-1].values
 y_set = data_1["return"].values
 y_set = y_set.reshape(-1,1)
-sc = MinMaxScaler(feature_range = (0, 1))
+sc = MinMaxScaler()
+#training_set_scaled =training_set
+#y_set_scaled=y_set
 training_set_scaled = sc.fit_transform(training_set)
 y_set_scaled = sc.fit_transform(y_set)
 
@@ -70,16 +67,19 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 
-hidden_layer_sizes = [100,100]
-def build_model(hidden_layer_sizes,act,input_d):
+def build_model(input_d):
   model = Sequential()
 
-  model.add(LSTM(units = hidden_layer_sizes[0],go_backwards= True,return_sequences= True,input_shape =input_d))
+  model.add(LSTM(units = 200,go_backwards= True,return_sequences= True,input_shape =input_d))
   model.add(Dropout(0.5))
 
-  model.add(LSTM(units=hidden_layer_sizes[len(hidden_layer_sizes)-1]))
+  model.add(LSTM(units=200, go_backwards=True, return_sequences=True))
   model.add(Dropout(0.5))
 
+  model.add(LSTM(units=200))
+  model.add(Dropout(0.5))
+
+  model.add(Dense(units=100))
   model.add(Dense(units=25))
   model.add(Dense(units=1))
 
@@ -89,7 +89,7 @@ def build_model(hidden_layer_sizes,act,input_d):
 
 
 
-regressor = build_model(hidden_layer_sizes,"relu",(X.shape[1],5))
+regressor = build_model((X.shape[1],5))
 
 
 #callbacks = [EarlyStopping(monitor='val_loss', patience=2),
@@ -119,22 +119,27 @@ y_set_scaled = sc.fit_transform(y_set)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 predicted_stock = pd.DataFrame(predicted_stock_price,columns=['perdict return'])
 real_stock = pd.DataFrame(real_stock_price,columns=['real return'])
+data_test = data[round(len(data)*factor_ratio):]
+data_test.reset_index(inplace=True)
+data_test = data_test[['Date']]
 
 final = pd.merge(real_stock, predicted_stock, left_index=True, right_index=True)
+final1 = pd.merge(data_test, final, left_index=True, right_index=True)
+final1.to_csv("{}.csv".format(company))
 final['direction'] = final["real return"]*final["perdict return"]
 def iden(x):
     if x >0:
         return 1
     else:
         return 0
-final["hit"] = final["direction"].apply(iden)
+final["hit"] = final["direction"].apply(lambda x:iden(x))
 print('hit ratio:{}'.format(final["hit"].sum()/final["hit"].count()))
 
-plt.plot(real_stock_price, color = 'black', label = 'AAPL Stock Price')
-plt.plot(predicted_stock_price, color = 'green', label = 'Predicted AAPL Stock return')
-plt.title('AAPL Stock return Prediction')
+plt.plot(real_stock_price, color = 'black', label = '{} Stock Price'.format(company))
+plt.plot(predicted_stock_price, color = 'green', label = 'Predicted {} Stock return'.format(company))
+plt.title('{} Stock return Prediction'.format(company))
 plt.xlabel('Time')
-plt.ylabel('AAPL Stock return')
+plt.ylabel('{} Stock return'.format(company))
 plt.legend()
 plt.show()
 
