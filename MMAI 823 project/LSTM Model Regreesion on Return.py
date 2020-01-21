31941695710b8e2666a57e6631210d7b3a8ce0b5
y437li <li.mac.kaka@gmail.com>
2020-01-21 18:38:22 -0500

    Update LSTM Model Regreesion on Return.py
    adding fixed seed
    change the data return to log normal
    comment out scale for output
    change 252 days to 30 days
    add one more dense laryer to lstm
    changed droppout ratio from 0.5 to 0.01
    use hidden units 200

--- MMAI 823 project/LSTM Model Regreesion on Return.py
+++ MMAI 823 project/LSTM Model Regreesion on Return.py
@@ -12,6 +12,15 @@ from keras import metrics
 import keras as ks
 import pickle
 import pandas_datareader.data as pd_reader
+import random as rn
+# Setting the seed for numpy-generated random numbers
+np.random.seed(37)
+
+# Setting the seed for python random numbers
+rn.seed(1254)
+
+# Setting the graph-level random seed.
+tf.random.set_seed(42)
 
 device_lib.list_local_devices()
 K.tensorflow_backend._get_available_gpus()
@@ -32,7 +41,8 @@ data = pd_reader.DataReader(
 )
 data = data[['Adj Close','High','Open','Low','Volume']]
 #data.sort_values(by='Date', ascending=True,inplace =True)
-data["return"] = (data["Adj Close"]-data["Adj Close"].shift(1))/data["Adj Close"].shift(1)
+#data["return"] = (data["Adj Close"]-data["Adj Close"].shift(1))/data["Adj Close"].shift(1)
+data["return"] = np.log(1+((data["Adj Close"]-data["Adj Close"].shift(1))/data["Adj Close"].shift(1)))
 #data["movement"] = data["return"].apply(lambda x:lable(x,tresh_hold))
 data = data.dropna()
 #data.drop(['return'],axis=1,inplace = True)
@@ -48,15 +58,16 @@ y_set = data_1["return"].values
 y_set = y_set.reshape(-1,1)
 sc = MinMaxScaler()
 #training_set_scaled =training_set
-#y_set_scaled=y_set
+
+y_set_scaled=y_set
 training_set_scaled = sc.fit_transform(training_set)
-y_set_scaled = sc.fit_transform(y_set)
+#y_set_scaled = sc.fit_transform(y_set)
 
 
 X = []
 y = []
-for i in range(252, len(training_set_scaled)):
-    X.append(training_set_scaled[i-252:i])
+for i in range(30, len(training_set_scaled)):
+    X.append(training_set_scaled[i-30:i])
     y.append(y_set_scaled[i][0])
 
 X, y = np.array(X), np.array(y)
@@ -70,17 +81,14 @@ from keras.layers import Dropout
 def build_model(input_d):
   model = Sequential()
 
-  model.add(LSTM(units = 200,go_backwards= True,return_sequences= True,input_shape =input_d))
-  model.add(Dropout(0.5))
+  model.add(LSTM(units = 250,go_backwards= True,return_sequences= True,input_shape =input_d))
+  model.add(Dropout(0.01))
 
-  model.add(LSTM(units=200, go_backwards=True, return_sequences=True))
-  model.add(Dropout(0.5))
+  model.add(LSTM(units=250))
+  model.add(Dropout(0.01))
 
-  model.add(LSTM(units=200))
-  model.add(Dropout(0.5))
-
-  model.add(Dense(units=100))
-  model.add(Dense(units=25))
+  model.add(Dense(units=10))
+  model.add(Dense(units=5))
   model.add(Dense(units=1))
 
   model.compile(optimizer='adam', loss='mean_squared_error')
@@ -95,7 +103,7 @@ regressor = build_model((X.shape[1],5))
 #callbacks = [EarlyStopping(monitor='val_loss', patience=2),
              #ModelCheckpoint(filepath='best_model.h5', monitor='val_loss', save_best_only=True)]
 
-model = regressor.fit(X, y, epochs = 100, batch_size = 100)
+model = regressor.fit(X, y, epochs =100, batch_size = 100)
 plt.plot(model.history['loss'])
 plt.show()
 
@@ -104,19 +112,19 @@ plt.show()
 real_stock_price = data_2['return'].values
 
 dataset_total = data.iloc[:,0:-1]
-inputs = dataset_total[len(dataset_total) - len(data_2) -252:].values
+inputs = dataset_total[len(dataset_total) - len(data_2) -30:].values
 #inputs = inputs.reshape(-1,1)
 inputs = sc.fit_transform(inputs)
 
 X_test = []
-for i in range(252, len(inputs)):
-    X_test.append(inputs[i-252:i])
+for i in range(30, len(inputs)):
+    X_test.append(inputs[i-30:i])
 
 X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 5))
 predicted_stock_price= regressor.predict(X_test)
-y_set_scaled = sc.fit_transform(y_set)
-predicted_stock_price = sc.inverse_transform(predicted_stock_price)
+#y_set_scaled = sc.fit_transform(y_set)
+#predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 predicted_stock = pd.DataFrame(predicted_stock_price,columns=['perdict return'])
 real_stock = pd.DataFrame(real_stock_price,columns=['real return'])
 data_test = data[round(len(data)*factor_ratio):]
